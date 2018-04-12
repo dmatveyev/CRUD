ac2a196ec19445f516aba0c8430d41117b3ea1b2
dmatveev <denis.matveev@magenta_technology.com>
2018-04-12 12:27:15 +0400

    print users

--- src/main/java/servlets/UserServlet.java
+++ src/main/java/servlets/UserServlet.java
@@ -5,6 +5,7 @@ import managers.UsersManager;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +14,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+@WebServlet(urlPatterns = "/",name = "UserServlet")
 public class UserServlet extends HttpServlet {
     private UsersManager usersManager;
 
