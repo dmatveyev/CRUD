706a2a44b402461e18b42e1ea75e5e6b7d498c62
dmatveev <denis.matveev@magenta_technology.com>
2018-04-12 16:21:20 +0400

    createservlet

--- src/main/java/servlets/CreateUserServlet.java
+++ src/main/java/servlets/CreateUserServlet.java
@@ -1,6 +1,25 @@
 package servlets;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+
+@WebServlet(urlPatterns = "/create-user",name = "createUserServlet")
 public class CreateUserServlet  extends HttpServlet {
+    @Override
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        
+    }
+
+    @Override
+    protected void doPost(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
