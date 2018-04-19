d91954dc51d5cef70242bd127591ca40cfa2d217
Денис <denismatveyevaleksandrovich@gmail.com>
2018-04-20 00:18:33 +0400

    UserController

---
+++ src/main/java/com/denis/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.denis.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    protected String doGet()  {
+        return "user";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    protected String doPost() {
+        return "user";
+    }
+}
