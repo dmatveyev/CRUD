8d03644d347f1c130a32be36ab31cf62f45f67e8
Денис <denismatveyevaleksandrovich@gmail.com>
2018-04-20 01:11:15 +0400

    Spring security 1

--- src/main/java/com/denis/config/SecurityInit.java
+++ src/main/java/com/denis/config/SecurityInit.java
@@ -1,4 +1,30 @@
 package com.denis.config;
 
-public class SecurityInit extends AbstractSecurityWebApplicationInitializer  {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+
+    public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Autowired
+        public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication().withUser("user").password("user").roles("USER");
+            auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+            auth.inMemoryAuthentication().withUser("superadmin").password("superadmin").roles("SUPERADMIN");
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+
+            http.authorizeRequests()
+                    .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                    .antMatchers("/user/**").access("hasRole('ROLE_SUPERADMIN')")
+                    .and().formLogin().defaultSuccessUrl("/", false);
+
+        }
+    }
 }
---
+++ src/main/java/com/denis/model/UserRole.java
@@ -0,0 +1,7 @@
+package com.denis.model;
+
+public enum UserRole {
+    ADMIN,
+    USER,
+
+}
