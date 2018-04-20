c773dea5843a89be76e0f013332b14c44f5f7389
Денис <denismatveyevaleksandrovich@gmail.com>
2018-04-21 00:02:40 +0400

    Refactoring 3

--- src/main/java/com/denis/config/AppConfig.java
+++ src/main/java/com/denis/config/AppConfig.java
@@ -3,12 +3,9 @@ package com.denis.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
 
 @Configuration
-@ComponentScan(basePackages = { "com.denis" })
+@ComponentScan(basePackages = {"com.denis"})
 @Import({OrmConfig.class, WebConfig.class, SecurityConfig.class})
 public class AppConfig {
-
 }
