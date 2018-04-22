5f8d96a5aadc6afedaa66835e60c2f12fc07aec6
Денис <denismatveyevaleksandrovich@gmail.com>
2018-04-23 01:07:04 +0400

    Application Properties

--- src/main/java/com/denis/config/SpringDataConfig.java
+++ src/main/java/com/denis/config/SpringDataConfig.java
@@ -25,27 +25,14 @@ import java.util.Properties;
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages = "com.denis")
-@PropertySource({ "classpath:application.properties" })
 @EnableJpaRepositories(basePackages="com.denis.repository")
 public class SpringDataConfig {
 
     @Autowired
     private Environment env;
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory (){
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setDatabase(Database.SQL_SERVER);
-        vendorAdapter.setGenerateDdl(true);
 
-        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource());
-        em.setPackagesToScan("com.denis.model");
-        em.setJpaVendorAdapter(vendorAdapter);
-        em.setJpaProperties(additionalProperties());
-        return em;
-    }
 
-    @Bean
+   @Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(env.getProperty("hibernate.connection.driver_class"));
@@ -55,24 +42,4 @@ public class SpringDataConfig {
         return dataSource;
     }
 
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(emf);
-
-        return transactionManager;
-    }
-
-    @Bean
-    public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
-        return new PersistenceExceptionTranslationPostProcessor();
-    }
-
-    Properties additionalProperties() {
-        Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
-        properties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
-        properties.setProperty("hibernate.current_session_context_class", env.getProperty("hibernate.current_session_context_class"));
-        return properties;
-    }
 }
