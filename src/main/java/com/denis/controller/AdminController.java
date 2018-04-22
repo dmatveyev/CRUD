07d8696bbaefd7bbc0c0463774f4f7a5a74d01b9
Денис <denismatveyevaleksandrovich@gmail.com>
2018-04-23 00:01:24 +0400

    Get login after autorization

--- src/main/java/com/denis/controller/AdminController.java
+++ src/main/java/com/denis/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.denis.controller;
 import com.denis.model.User;
 import com.denis.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,12 +31,15 @@ public class AdminController {
 
     @RequestMapping(method = RequestMethod.GET)
     public String doGet(ModelMap mapModel) {
+        UserDetails userDetails = (UserDetails) org.springframework.security.core.context.SecurityContextHolder
+                .getContext().getAuthentication().getPrincipal();
+        String userName = userDetails.getUsername();
         log.info("In admin controller");
         List<User> users = usersService.getAll();
         for (User u : users) {
             log.info(u.toString());
         }
-
+        mapModel.addAttribute("userName", userName);
         mapModel.addAttribute("users", users);
         log.info("returning admin.html");
         return "/admin";
--- src/main/java/com/denis/controller/UserController.java
+++ src/main/java/com/denis/controller/UserController.java
@@ -2,12 +2,12 @@ package com.denis.controller;
 
 import com.denis.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/user")
@@ -22,7 +22,10 @@ public class UserController {
 
 
     @RequestMapping(method = RequestMethod.GET)
-    protected String doGet() {
+    protected String doGet(ModelMap modelMap) {
+        UserDetails userDetails = (UserDetails) org.springframework.security.core.context.SecurityContextHolder
+                .getContext().getAuthentication().getPrincipal();
+        modelMap.addAttribute("userName", userDetails.getUsername());
         return "user";
 
     }
