2119ddd01d60f9b2fb066ae1b7cab1f07e1be589
Денис <denismatveyevaleksandrovich@gmail.com>
2018-04-29 19:54:16 +0400

    Simple Google OAuth

--- Application/src/main/java/com/denis/Application.java
+++ Application/src/main/java/com/denis/Application.java
@@ -2,9 +2,11 @@ package com.denis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class Application {
     public Application(){}
 
