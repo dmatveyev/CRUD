8d3af91eb8315cca5201490d971be8239eebe20b
Денис <denismatveyevaleksandrovich@gmail.com>
2018-04-29 13:58:32 +0400

    ""

--- src/main/java/com/denis/Application.java
+++ src/main/java/com/denis/Application.java
@@ -2,7 +2,6 @@ package com.denis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
@@ -11,5 +10,6 @@ public class Application {
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
+
     }
 }
