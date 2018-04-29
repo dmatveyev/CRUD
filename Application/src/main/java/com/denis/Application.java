f33a14ae837fda60931bbf281ab24e96a48858b3
Денис <denismatveyevaleksandrovich@gmail.com>
2018-04-29 20:07:13 +0400

    disable csrf

--- Application/src/main/java/com/denis/Application.java
+++ Application/src/main/java/com/denis/Application.java
@@ -3,15 +3,31 @@ package com.denis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class Application {
-    public Application(){}
+public class Application extends WebSecurityConfigurerAdapter {
+    public Application() {
+    }
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
 
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+        http.csrf().disable();
     }
 }
