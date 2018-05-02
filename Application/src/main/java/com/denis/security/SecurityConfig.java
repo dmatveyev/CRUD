01cacf68a5e66a87cfa46a6c26461e1fe467247e
Денис <denismatveyevaleksandrovich@gmail.com>
2018-05-02 20:28:58 +0400

    Hello world telegram bot

--- Application/src/main/java/com/denis/security/SecurityConfig.java
+++ Application/src/main/java/com/denis/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.denis.security.handler.FailureHandler;
 import com.denis.security.handler.SecurityHandler;
 import com.denis.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -33,6 +34,7 @@ import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
+
 @PropertySource("classpath:application.properties")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
--- CRUDClient/src/main/java/com/client/Client.java
+++ CRUDClient/src/main/java/com/client/Client.java
@@ -1,13 +1,24 @@
 package com.client;
 
+import com.client.bot.Bot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.client.repository")
 public class Client {
     public static void main(final String[] args) {
-        SpringApplication.run(Client.class, args);
+        ApiContextInitializer.init(); // Инициализируем апи
+        TelegramBotsApi botapi = new TelegramBotsApi();
+        try {
+            botapi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
