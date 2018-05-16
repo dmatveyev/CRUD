1691afaa9b8185cfebc7e7400c8b613828b6bf2d
dmatveev <denis.matveev@magenta_technology.com>
2018-05-16 16:52:41 +0400

    selenium autotest

--- Application/src/test/java/com/denis/ApplicationTest.java
+++ Application/src/test/java/com/denis/ApplicationTest.java
@@ -17,6 +17,7 @@ import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
 
+
 public class ApplicationTest {
 
     public static WebDriver driver;
@@ -25,9 +26,10 @@ public class ApplicationTest {
     public static void setUp() {
         System.setProperty("webdriver.chrome.driver", "D:/Denis Matveev/chromedriver/chromedriver.exe");
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://localhost:8080/login");
     }
+
     @AfterClass
     public static void tearDown() {
         driver.quit();
@@ -35,28 +37,20 @@ public class ApplicationTest {
 
     @Test
     public void firstTest() throws InterruptedException {
-        WebElement loginButton =  driver.findElement(By.id("google"));
+        WebElement loginButton = driver.findElement(By.id("google"));
         loginButton.click();
 
         //login by Google OAuth
         //Click GMail login
         driver.findElement(By.id("identifierId")).sendKeys("denismatveyevaleksandrovich@gmail.com");
         driver.findElement(By.id("identifierNext")).click();
-
-
         driver.findElement(By.name("password")).sendKeys("123qwe!@#QWE)");
-
-
         /*WebDriverWait wait2 = new WebDriverWait(driver, 10);
         wait2.until(ExpectedConditions.elementToBeClickable(By.id("passwordNext"))).click();*/
         WebElement myelement = driver.findElement(By.id("passwordNext"));
-        JavascriptExecutor jse2 = (JavascriptExecutor)driver;
-        jse2.executeScript("arguments[0].click();", myelement);
-
-        WebElement header = driver.findElement(By.linkText("CRUD"));
-
-        String adminUrl = driver.getCurrentUrl();
-        assertEquals(adminUrl, "http://localhost:8080/admin#");
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("arguments[0].click();", myelement);
+        driver.findElement(By.linkText("CRUD"));
+        assertEquals(driver.getCurrentUrl(), "http://localhost:8080/admin#");
     }
-
 }
